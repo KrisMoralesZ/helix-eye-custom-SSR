81927d01c24cc7d0077bf2b7735ba82c121feedf
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-04-25 14:11:30 -0700

    First express server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello wold");
+import express, { Express, Request, Response } from "express";
+import { config } from "./config";
+
+const app: Express = express();
+
+app.get("*", (req: Request, res: Response) => {
+  res.send(`<h1>Hello World from: ${req.url}</h1>`);
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`);
+});
