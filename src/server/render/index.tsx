dafa28aec875281efa59ac73b938865a380450eb
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-04-30 15:37:39 -0700

    Add styleTags

--- src/server/render/index.tsx
+++ src/server/render/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 import { App } from "../../app/containers/App";
 import { StaticRouter } from "react-router-dom/server";
 import { renderToString } from "react-dom/server";
 import { template } from "./template";
 
 export const render = (url: string, initialProps = {}) => {
-  const stream = renderToString(
-    <StaticRouter location={url}>
-      <App />
-    </StaticRouter>
-  );
+  const sheet = new ServerStyleSheet();
 
-  const html = template(stream, initialProps);
+  try {
+    const stream = renderToString(
+      sheet.collectStyles(
+        <StaticRouter location={url}>
+          <App />
+        </StaticRouter>
+      )
+    );
 
-  return html;
+    const styleTags = sheet.getStyleTags();
+
+    const html = template(stream, initialProps, styleTags);
+
+    return html;
+  } catch (error) {
+    console.log(error);
+  }
 };
